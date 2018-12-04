b1c5220261b9e621b431fd0303323999e041ea57
Jean Fontena <diegofontena@gmail.com>
2018-12-04 11:49:46 -0200

    Output the insertion time and the tree that's being used

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 // C
 #include <stdio.h>
+#include <time.h>
 // Local
 #include "files.h"
 #include "parser.h"
@@ -11,6 +12,7 @@ int main(int argc, char** argv) {
     if (!is_argc_valid(argc)) {
         fprintf(stderr, "Invalid usage!\n");
         fprintf(stderr, " Usage: %s lexical.txt sentences.txt output.txt", argv[0]);
+        return -1;
     }
 
     FILE *lexicon = NULL;
@@ -21,15 +23,26 @@ int main(int argc, char** argv) {
     if (status != OK) {
         return status;
     }
+#ifdef AVL_TREE
+    printf("Running with AVL tree\n");
+#else
+    printf("Running with Binary Tree\n");
+#endif
 
     Node *tree2 = new_tree();
-    // full fill the tree
+
+    // full fill the tree and calculate the time taken
+    clock_t start = clock();
     handle_lexicon(&lexicon, &tree2);
+    const double insert_time = (double) (clock() - start) / CLOCKS_PER_SEC;
+
     // search for the words in the tree
     handle_sentences(&sentences, &output, &tree2);
+
     // prints the number of compares
     printf("Ok! Output file %s generated successfully.", get_filename(OUTPUT, argv));
     printf("\nComparisons: %d", comp);
+    printf("\nInsertion time: %f", insert_time);
     printf("\n");
     return 0;
 }
